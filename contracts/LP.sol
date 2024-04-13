d3e7125d81e6f1db4dd96c7e25d819e57b10136a
ioanstoica <ioanstoica89@gmail.com>
2024-04-14 00:18:02 +0300

    Liquidity Pool

---
+++ contracts/LP.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+contract LP {
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+    uint256 public reserveA;
+    uint256 public reserveB;
+
+    constructor(IERC20 _tokenA, IERC20 _tokenB) {
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+    }
+
+    function addLiquidity(uint256 amountA, uint256 amountB) public {
+        tokenA.transferFrom(msg.sender, address(this), amountA);
+        tokenB.transferFrom(msg.sender, address(this), amountB);
+        reserveA += amountA;
+        reserveB += amountB;
+    }
+
+    function removeLiquidity(uint256 amountA, uint256 amountB) public {
+        require(
+            reserveA >= amountA && reserveB >= amountB,
+            "Insufficient liquidity"
+        );
+        IERC20(tokenA).transfer(msg.sender, amountA);
+        IERC20(tokenB).transfer(msg.sender, amountB);
+        reserveA -= amountA;
+        reserveB -= amountB;
+    }
+
+    function getReturn(
+        IERC20 fromToken,
+        uint256 amountIn
+    ) public view returns (uint256) {
+        require(
+            (fromToken == tokenA || fromToken == tokenB),
+            "Invalid token address"
+        );
+        if (fromToken == tokenA) {
+            return (amountIn * reserveB) / (reserveA + amountIn);
+        } else {
+            return (amountIn * reserveA) / (reserveB + amountIn);
+        }
+    }
+
+    function swap(IERC20 fromToken, uint256 amountIn) public {
+        uint256 amountOut = getReturn(fromToken, amountIn);
+        require(amountOut > 0, "Invalid swap amount");
+
+        if (fromToken == tokenA) {
+            IERC20(tokenA).transferFrom(msg.sender, address(this), amountIn);
+            IERC20(tokenB).transfer(msg.sender, amountOut);
+            reserveA += amountIn;
+            reserveB -= amountOut;
+        } else {
+            IERC20(tokenB).transferFrom(msg.sender, address(this), amountIn);
+            IERC20(tokenA).transfer(msg.sender, amountOut);
+            reserveB += amountIn;
+            reserveA -= amountOut;
+        }
+    }
+}
---
+++ test/LPTests.js
@@ -0,0 +1,77 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("LP Tests", function () {
+    async function deployContract(tokenA, tokenB) {
+        const LP = await ethers.getContractFactory("LP");
+        const LPContract = await LP.deploy(tokenA, tokenB);
+        return LPContract;  // Directly return the deployed contract
+    }
+
+    async function deployERC20ContractOK() {
+        const initialTokens = 10 ** 6;
+        const ERC20 = await ethers.getContractFactory("ERC20");
+
+        const [owner, user2, user3, user4, user5] = await ethers.getSigners();
+
+        const erc20Contract = await ERC20.connect(owner).deploy(initialTokens);
+
+        return { erc20Contract, owner, user2, user3, user4, user5 };
+    }
+
+    it("Should deploy LP contract", async function () {
+        const { erc20Contract: tokenA } = await deployERC20ContractOK();
+        const { erc20Contract: tokenB } = await deployERC20ContractOK();
+
+        const LP = await deployContract(tokenA, tokenB);
+
+        // Validate that the contracts have been deployed
+        expect(LP).to.not.be.undefined;
+    });
+
+    it("Should add liquidity", async function () {
+        const { erc20Contract: tokenA } = await deployERC20ContractOK();
+        const { erc20Contract: tokenB } = await deployERC20ContractOK();
+
+        const LP = await deployContract(tokenA, tokenB);
+
+        const amountA = 1000;
+        const amountB = 1000;
+
+        await tokenA.approve(LP.target, amountA);
+        await tokenB.approve(LP.target, amountB);
+
+        await LP.addLiquidity(amountA, amountB);
+
+        const LPBalanceA = await tokenA.balanceOf(LP.target);
+        const LPBalanceB = await tokenB.balanceOf(LP.target);
+
+        expect(LPBalanceA).to.equal(1000);
+        expect(LPBalanceB).to.equal(1000);
+    });
+
+    it("Should remove liquidity", async function () {
+        const { erc20Contract: tokenA } = await deployERC20ContractOK();
+        const { erc20Contract: tokenB } = await deployERC20ContractOK();
+
+        const LP = await deployContract(tokenA, tokenB);
+
+        const amountA = 1000;
+        const amountB = 1000;
+
+        await tokenA.approve(LP.target, amountA);
+        await tokenB.approve(LP.target, amountB);
+
+        await LP.addLiquidity(amountA, amountB);
+
+        await LP.removeLiquidity(1000, 1000);
+
+        const LPBalanceA = await tokenA.balanceOf(LP.target);
+        const LPBalanceB = await tokenB.balanceOf(LP.target);
+
+        expect(LPBalanceA).to.equal(0);
+        expect(LPBalanceB).to.equal(0);
+    });
+
+    
+});
